9c36cfca2bb760c71c0e2833698a833382acbdc0
Ricardo Lazaro <ricardoalazaro@hotmail.com>
2023-07-12 12:29:06 -0300

    OrderItem, OrderItemPK modidication Product, Order-ManyToMany

--- src/main/java/com/ricardolazaro/DSComerce/entities/Order.java
+++ src/main/java/com/ricardolazaro/DSComerce/entities/Order.java
@@ -3,6 +3,8 @@ package com.ricardolazaro.DSComerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="tb_order")
@@ -23,6 +25,8 @@ public class Order {
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
 
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
     public Order(){
     }
 
